c9835599d985ba9ee50024af431479d5a7123af8
Sachin <sachin31301@gmail.com>
2021-06-17 16:52:21 +0530

    finaling

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,3 +1,5 @@
 module.exports.profile= function(req,res){
-    return res.end('<h1>I am user</h1>')
+    return res.render('user',{
+      title:"Codeial"
+    })
   }
--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const express= require('express')
 const app=express();
 const port =8000
 
+const expLayout=require('express-ejs-layouts')
+app.use(express.static('./assets'))
+app.use(expLayout)
+app.set('layout extractStyles',true)
+app.set('layout extractScripts',true)
+
 app.use('/',require('./routes'))
 
 app.set('view engine', 'ejs')
